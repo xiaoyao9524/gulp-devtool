ede7e2da59ad2f823528e334313750ee4528030e
xiaoyao <xiaoyao9524@163.com>
2018-12-18 14:33:57 +0800

    使用async、await重构代码、使用gulp-babel转换es6代码

--- gulpfile.js
+++ gulpfile.js
@@ -6,115 +6,129 @@ const reload = browserSync.reload;
 const del = require('del');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const combiner = require('stream-combiner2');
-const useref = require('gulp-useref');
+const babel = require("gulp-babel");
+// const combiner = require('stream-combiner2');
+// const useref = require('gulp-useref');
 const log = log => {
   console.log(`👉👉👉${log}👈👈👈`);
 };
-
 // 开发部分
-gulp.task('server', ['devScss'], () => {
+gulp.task('server', async () => {
+  log('正在启动服务...');
+  await buildScss();
+  await compilerJS('./src/js/es6/*.js', './src/js/es5/');
+
   browserSync.init({
     server: './src',
     port: 8080, // 设置端口
     ghostMode: false, // 开启的话有一个设备 滚动(scroll)\点击(clicks)\forms 了页面其他设备也会有相同的行为
     notify: false // 不会在浏览器里显示任何提醒(开启的话每次刷新会有提示，在移动端下遮挡很严重)
   });
-  gulp.watch('src/scss/*.scss', ['devScss'])
+  gulp.watch('src/scss/*.scss', buildScss);
+  gulp.watch('src/js/es6/*.js', () => {
+    compilerJS('./src/js/es6/*.js', './src/js/es5/');
+  });
+
   gulp.watch([
-    'src/*.html', 
+    'src/*.html',
     'src/page/*.html',
     'src/css/*.css',
-    'src/js/*.js',
+    'src/js/es5/*.js',
     'src/img/*'
   ])
-    .on('change', reload)
-});
-
-gulp.task('devScss', function () {
-  return combiner([
-    gulp.src('./src/scss/*.scss'),
-    sass(),
-    autoprefixer({
-      browsers: ['last 2 Explorer versions', 'Firefox >= 20', 'last 100 versions'],
-      cascade: true
-    }),
-    gulp.dest('./src/css/')
-  ]).on('error', console.error.bind(console));
+      .on('change', reload);
 });
 
 // 构建部分
-gulp.task('build', ['clearDist', 'scss', 'prefixer', 'buildFile'], () => {
-  log('构建完成')
-})
+gulp.task('build', async () => {
+  log('开始构建：');
+  await clearDist();
+  await buildScss();
+  await prefixer();
+  await compilerJS('./src/js/es6/*.js', './src/js/es5/');
+  await buildFile();
+  log('构建完成');
+});
 
-gulp.task('clearDist', () => {
-  return new Promise((resolve) => {
-    log('清除dist')
+function clearDist () {
+  return new Promise(resolve => {
+    log('开始清除dist');
     del(['dist'])
       .then(() => {
-        log('清除完成')
+        log('清除dist完成');
         resolve();
-      })
+      });
   })
-})
-
-gulp.task('scss', ['clearDist'], () => {
-  log('开始编译scss')
+}
+function buildScss() {
+  log('开始编译scss');
   return new Promise((resolve) => {
     gulp.src('./src/scss/*.scss')
-      .pipe(sass({
-          outputStyle: 'expanded'
-        })
-        .on('error: ', sass.logError))
-      .pipe(gulp.dest('./src/css/'))
-      .on('end', () => {
-        log('scss编译完成');
-        resolve();
-      })
+        .pipe(sass())
+        .pipe(
+            autoprefixer(
+                {
+                  browsers: ['last 2 Explorer versions', 'Firefox >= 20', 'last 100 versions'],
+                  cascade: true
+                }
+            )
+        )
+        .pipe(gulp.dest('./src/css/'));
+    log('编译scss成功！');
+    resolve();
   })
-});
-
-gulp.task('prefixer', ['scss'], () => {
-  log('autoprefixer 开始')
+}
+function prefixer () {
   return new Promise((resolve) => {
+    log('开始添加css前缀:');
     gulp.src('./src/css/*.css')
-      .pipe(autoprefixer({
-        browsers: ['last 2 Explorer versions', 'Firefox >= 20', 'last 100 versions'],
-        cascade: false
-      }))
-      .pipe(gulp.dest('./src/css/'))
-      .on('end', () => {
-        log('autoprefixer完成');
-        resolve();
-      })
+        .pipe(autoprefixer({
+          browsers: ['last 2 Explorer versions', 'Firefox >= 20', 'last 100 versions'],
+          cascade: false
+        }))
+        .pipe(gulp.dest('./src/css/'))
+        .on('end', () => {
+          log('添加css前缀完成');
+          resolve();
+        })
   })
-
-});
-
-gulp.task('buildFile', ['prefixer'], () => {
-  log('打包文件')
-  // 写入index.html
-  gulp.src('./src/*.html')
-    .pipe(useref())
-    .pipe(gulp.dest('./dist/'))
-  // 写入page
-  gulp.src('./src/page/*.html')
-    .pipe(useref())
-    .pipe(gulp.dest('./dist/page/'))
-  // 写入img
-  gulp.src('./src/img/*.html')
-    .pipe(gulp.dest('./dist/img/'))
-  // 写入css
-  gulp.src('./src/css/*.css')
-    .pipe(gulp.dest('./dist/css/'))
-  // 写入js
-  gulp.src('./src/js/*.js')
-    .pipe(gulp.dest('./dist/js/'))
-    .on('end', () => {
-      log('写入文件完成')
-    })
-})
+}
+function compilerJS (readFilePath = './src/js/es6/*.js', outFilePath = './src/js/es5/') {
+  return new Promise(resolve => {
+    log('开始转换js');
+    gulp.src(readFilePath)
+        .pipe(babel())
+        .pipe(gulp.dest(outFilePath));
+    log('转换js成功');
+    resolve();
+  })
+}
+function buildFile () {
+  return new Promise(resolve => {
+    log('开始写入文件：');
+    // 写入index.html
+    gulp.src('./src/*.html')
+    // .pipe(useref())
+        .pipe(gulp.dest('./dist/'));
+    // 写入page
+    gulp.src('./src/page/*.html')
+    // .pipe(useref())
+        .pipe(gulp.dest('./dist/page/'));
+    // 写入img
+    gulp.src('./src/img/*')
+        .pipe(gulp.dest('./dist/img/'));
+    // 写入css
+    gulp.src('./src/css/*.css')
+        .pipe(gulp.dest('./dist/css/'));
+    // 写入js
+    gulp.src('./src/js/es5/*.js')
+        .pipe(gulp.dest('./dist/js/es5/'))
+        .on('end', () => {
+          log('写入文件完成！');
+          resolve();
+        })
+  });
+}
 
 /*
   outputStyle:
---
+++ src/js/es6/index.js
@@ -0,0 +1,9 @@
+console.log('index1.js');
+document.querySelector('.text').innerText = `js文件引入成功！！！`;
+
+let [a, b] = ['测试解构', '赋值'];
+console.log(a, b);
+
+let f = 'xiao';
+let l = 'yao';
+console.log(`Hello ${f} - ${l}！`);
